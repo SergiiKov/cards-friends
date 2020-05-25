a7c2e5e85ea0cbcf2f3888f4428ed9d80b58b3c8
007 <yamatsuba008@gmail.com>
2020-05-25 22:20:01 +0300

    add serch component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/serch-box/serch-box.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      robots: [] 
+      robots: [],
+      searchField:''
     };
   }
 
@@ -17,12 +18,23 @@ class App extends Component {
     .then(users => this.setState({ robots:users }));
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
 
   render() {
-    return (
+   const { robots, searchField } = this.state;
+    const filterRobots = robots.filter(robot => 
+      robot.name.toLowerCase().includes(searchField.toLowerCase()))
+    return(
       <div className="App">
-       <CardList robots={this.state.robots} />
-       </div>
+        <h1>Robots friends</h1>
+        <SearchBox
+        placeholder='robot name' 
+        handleChange={this.handleChange}
+        />
+       <CardList robots={filterRobots} />   
+    </div>
     );
 
   }
